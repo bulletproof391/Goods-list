57601c0ad66fa9cc8c0a58a441646fb4f3e668e5
Dmitry <bulletproof024@gmail.com>
2018-01-13 12:11:29 +0700

    Initial project commit

---
+++ Goods list/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Goods list
+//
+//  Created by Дмитрий Вашлаев on 13.01.18.
+//  Copyright © 2018 Дмитрий Вашлаев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Goods list/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Goods list
+//
+//  Created by Дмитрий Вашлаев on 13.01.18.
+//  Copyright © 2018 Дмитрий Вашлаев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
